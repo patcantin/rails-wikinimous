1529c92753567b300d4300886f271399c7fbeb95
patcantin <patcantin23@gmail.com>
2020-01-27 14:12:46 -0500

    wikinimous final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Article.create(title: 'Colors', content: 'Ice-t made the soundtrack')
+Article.create(title: 'Kobe', content: 'How it happens?')
+Article.create(title: 'Daft punk', content: 'Why masks?')
+Article.create(title: 'Flying', content: 'Shoping filghts')
+Article.create(title: 'Pop locking', content: 'How it all started')
+Article.create(title: 'Bushwick Bill', content: 'A brief history')
+Article.create(title: 'Prague', content: 'It is so cheap?')
+Article.create(title: 'RollerBlade', content: 'RollerBlade in the 90s')
+Article.create(title: 'Place Bonaventure', content: 'Before after')
+Article.create(title: 'Moonwalk', content: 'Boogaloo Schrimp')
+
